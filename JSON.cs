d587f5d45dcf7281e2536f82ea8ed543282a5dbd
Bruce Axtens <bruce.axtens@gmail.com>
2022-08-29 14:45:56 +0800

    Add project files.

---
+++ JSON.cs
@@ -0,0 +1,18 @@
+﻿using NetTools;
+
+namespace Whois
+{
+    internal class JSON
+    {
+        public object AddressRange { get; set; }
+        public object Raw { get; set; }
+        public object OrganizationName { get; set; }
+        public object RespondedServers { get; set; }
+        public object RegistrarRegistrationExpirationDate { get; internal set; }
+        public object DomainStatus { get; internal set; }
+        public object NameServers { get; internal set; }
+        public object AllParts { get; internal set; }
+        public object DomainServiceProvider { get; internal set; }
+        public string[] EndMatter { get; internal set; }
+    }
+}
---
+++ Whois.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using Whois.NET;
+
+namespace Whois
+{
+    public static class Whois
+    {
+        public static string Who_is(string domain, bool debug = false)
+        {
+            if (debug) Debugger.Launch();
+            var response = WhoisClient.Query(domain);
+            var (raw, endMatter) = RemoveEndMatter(response.Raw);
+            var json = new JSON();
+            json.AddressRange = response.AddressRange?.ToString();
+            json.Raw = (from line in response.Raw.Split(new char[] { '\r', '\n' }) where line.Length > 0 select line).ToArray();
+            json.OrganizationName = response.OrganizationName;
+            json.RespondedServers = response.RespondedServers;
+            json.RegistrarRegistrationExpirationDate = ExtractPartFromRaw("Registrar Registration Expiration Date", "", raw);
+            json.DomainStatus = ExtractPartFromRaw("Domain Status", "", raw).Split(' ')[0];
+            json.NameServers = ExtractPartFromRaw("Name Server", ",", raw).ToLower();
+            json.AllParts = ExtractAllPartsFromRaw(raw);
+            json.EndMatter = endMatter;
+            return JsonConvert.SerializeObject(json);
+        }
+
+        private static (string raw, string[] moreInformation) RemoveEndMatter(string raw)
+        {
+            var miFound = false;
+            var miList = new List<string>();
+            var lines = (from line in raw.Split(new char[] { '\r', '\n' }) where line.Length > 0 select line).ToArray();
+            var miFoundAt = lines.Count();
+            for (var i = 0; i < lines.Count(); i++)
+            {
+                var line = lines[i];
+                if (line.Contains(">>> Last update of WHOIS database"))
+                {
+                    miFound = true;
+                    miFoundAt = i;
+                }
+                if (miFound)
+                {
+                    miList.Add(line.Trim());
+                }
+            }
+            return (string.Join("\r\n", lines.Take(miFoundAt)), miList.ToArray());
+        }
+
+        private static (string raw, string[] domainServiceProvider) RemoveDomainServiceProvider(string raw)
+        {
+            var dsrFound = false;
+            var dsrList = new List<string>();
+            var lines = (from line in raw.Split(new char[] { '\r', '\n' }) where line.Length > 0 select line).ToArray();
+            var dsrFoundAt = lines.Count();
+            for (var i = 0; i < lines.Count(); i++)
+            {
+                var line = lines[i];
+                if (dsrFound)
+                {
+                    dsrList.Add(line.Trim());
+                }
+                if (line.Contains("Domain Service Provider:"))
+                {
+                    dsrFound = true;
+                    dsrFoundAt = i;
+                }
+            }
+            return (string.Join("\r\n", lines.Take(dsrFoundAt)), dsrList.ToArray());
+        }
+
+        private static Dictionary<string, List<string>> ExtractAllPartsFromRaw(string raw)
+        {
+            var result = new Dictionary<string, List<string>>();
+            raw = raw.Replace("For more information on Whois status codes, please visit", "More information: For more information on Whois status codes, please visit");
+            raw = raw.Replace(">>>", "").Replace("<<<", "");
+            var pairs = (from line
+                        in raw.Split(new char[] { '\r', '\n' })
+                         where line.Length > 0
+                         let pos = line.IndexOf(':')
+                         let pair = new string[] {
+                             pos == -1 ? line : line.Substring(0, pos).Trim(),
+                             pos == -1 ? "" : line.Substring(pos + 1).Trim()
+                         }
+                         select pair);
+            foreach (var pair in pairs)
+            {
+                if (result.ContainsKey(pair[0]))
+                {
+                    if (pair[1].Length > 0)
+                        result[pair[0]].Add(pair[1]);
+                }
+                else
+                {
+                    var l = new List<string>() { pair[1] };
+                    result[pair[0]] = l;
+                }
+            }
+            return result;
+        }
+
+        private static string ExtractPartFromRaw(string v, string sep, string raw)
+        {
+            return string.Join(sep, (from line
+                                    in raw.Split(new char[] { '\r', '\n' })
+                                     where line.Length > 0 && line.Contains(v)
+                                     select line.Substring(line.IndexOf(":") + 1).Trim()).ToArray());
+        }
+    }
+}
